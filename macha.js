440d7657882c0d7b636cb503eb0cc9b2b8686880
KELVIN BAHATI <102055576+machakelvin@users.noreply.github.com>
2023-05-25 12:33:48 +0000

    macha did

--- macha.js
+++ macha.js
@@ -1,11 +1,11 @@
 function display(value){
     document.getElementById("input").value+=value;
-}
+ }
 function calculate(){
     var p = document.getElementById("input").value;
     var q = eval(p);
     document.getElementById("input").value = q;
-}
+ }
 function clearScreen(){
     document.getElementById("input").value = "";
 }
